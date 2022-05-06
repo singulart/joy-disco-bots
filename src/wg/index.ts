a9d32cd515ecd2b0340ef7b37f4db5286f733493
lex <7863785+singulart@users.noreply.github.com>
2022-05-06 15:52:44 +0300

    addressed reviewers comment about mixing thenable with await

--- src/wg/index.ts
+++ src/wg/index.ts
@@ -7,28 +7,28 @@ import Discord, { Intents } from "discord.js";
 import { getDiscordChannels } from "../util";
 import { connectApi, getBlockHash, getEvents } from "./api_extension";
 import { processGroupEvents } from "./wg_event_handlers";
+import { banner } from "../banner";
 
 ; (async () => {
-
+  console.log(banner);
   const discordBotToken = process.env.TOKEN || undefined; // environment variable TOKEN must be set
 
   const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 
-    client.once("ready", async () => {
-      console.log('Discord.js client ready');
-      const channels: DiscordChannels = await getDiscordChannels(client);
-      const api: ApiPromise = await connectApi(wsLocation);
-      await api.isReady;
-      api.rpc.chain.subscribeFinalizedHeads(async (header: Header) => {
-        getBlockHash(api, +header.number).then((hash) =>
-          getEvents(api, hash).then((events: EventRecord[]) =>
-            processGroupEvents(+header.number, events, channels)
-          )
-        )
-      })
+  client.once("ready", async () => {
+    console.log('Bot ready');
+    const channels: DiscordChannels = await getDiscordChannels(client);
+    const api: ApiPromise = await connectApi(wsLocation);
+    await api.isReady;
+    console.log(`Connected to RPC endpoint [${wsLocation}]`);
+    api.rpc.chain.subscribeFinalizedHeads(async (header: Header) => {
+      const hash = await getBlockHash(api, +header.number);
+      const events = await getEvents(api, hash);
+      processGroupEvents(+header.number, events, channels);
     });
+  });
 
   client.login(discordBotToken).then(async () => {
-    console.log(`Bot logged in. Current server[s]: ${client.guilds.cache.map(g => g.name).join(', ')}`);
+    console.log(`Bot online. Current server[s]: ${client.guilds.cache.map(g => g.name).join(',')}`);
   });
 })()
