dcb4f926f9126f2df00411b149815b19e3910b31
singulart <serpian@gmail.com>
2022-06-08 13:45:02 +0300

    #757 forum bot basic notification flow complete

--- src/forum/forum.module.ts
+++ src/forum/forum.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ForumService } from './forum.service';
 import { DiscordModule } from '@discord-nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from 'src/gql/gql.module';
 
 @Module({
-  imports: [DiscordModule.forFeature(), ConfigModule.forRoot()],
+  imports: [
+    DiscordModule.forFeature(), 
+    ConfigModule.forRoot(),
+    GraphQLModule
+  ],
   providers: [ForumService],
 })
 export class ForumModule {}
