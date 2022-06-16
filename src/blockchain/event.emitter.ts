07a09eab5f2991799528e31c80f7366efd13f57b
lex <7863785+singulart@users.noreply.github.com>
2022-06-16 16:00:25 +0300

    New architecture with a single blockchain event loop and subscribing modules. Forum module migrated to this architecture already

---
+++ src/blockchain/event.emitter.ts
@@ -0,0 +1,33 @@
+import { Once } from "@discord-nestjs/core";
+import { Injectable, Logger } from "@nestjs/common";
+import { wsLocation } from "config";
+import { connectApi, getBlockHash, getEvents } from "src/util";
+import { ApiPromise } from "@polkadot/api";
+import { Header, EventRecord } from '@polkadot/types/interfaces'
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { EventWithBlock } from "src/types";
+
+
+@Injectable()
+export class EventEmitterService {
+  private readonly logger = new Logger(EventEmitterService.name);
+
+  constructor(
+    private eventEmitter: EventEmitter2
+  ) { }
+
+  @Once('ready')
+  async onReady(): Promise<void> {
+    const api: ApiPromise = await connectApi(wsLocation);
+    await api.isReady;
+    this.logger.log(`Event Emitter connected to RPC endpoint [${wsLocation}]`);
+    api.rpc.chain.subscribeFinalizedHeads(async (header: Header) => {
+      const hash = await getBlockHash(api, +header.number);
+      const events = await getEvents(api, hash);
+      events.forEach((e: EventRecord) => {
+        let { section, method } = e.event;
+        this.eventEmitter.emit(`${section}.${method}`, {event: e, block: +header.number} as EventWithBlock); 
+      });
+    });
+  }
+}
--- src/forum/forum.module.ts
+++ src/forum/forum.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ForumService } from './forum.service';
 import { DiscordModule } from '@discord-nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from 'src/gql/gql.module';
+import { ThreadCreatedHandler } from './thread.created.handler';
+import { PostCreatedHandler } from './post.created.handler';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     DiscordModule.forFeature(), 
+    EventEmitterModule.forRoot(),
     ConfigModule.forRoot(),
     GraphQLModule
   ],
-  providers: [ForumService],
+  providers: [ThreadCreatedHandler, PostCreatedHandler],
 })
 export class ForumModule {}
