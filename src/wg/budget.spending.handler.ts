dd44e83146064059da1a906e2b0576eee3bb2cc6
lex <7863785+singulart@users.noreply.github.com>
2022-06-26 21:13:51 +0300

    added support for discretionary spendings to increase transparency of DAO operations

---
+++ src/wg/budget.spending.handler.ts
@@ -0,0 +1,37 @@
+import { Balance } from "@joystream/types/common";
+import { Injectable} from "@nestjs/common";
+import { OnEvent } from "@nestjs/event-emitter";
+import { TextChannel } from "discord.js";
+import { EventWithBlock } from "src/types";
+import { BaseEventHandler } from "./base.event.handler";
+import { getDiscretionarySpendingEmbed, getDiscretionarySpendingToNonWorkerAddressEmbed } from "./embeds";
+
+@Injectable()
+export class BudgetSpendingHandler extends BaseEventHandler {
+
+  @OnEvent('*.BudgetSpending')
+  async handleBudgetSpendingEvent(payload: EventWithBlock) {
+    let { section, data } = payload.event.event;
+    if (!this.checkChannel(section)) {
+      return;
+    }
+    const payee = data[0].toString();
+    const spendingAmount = data[1] as Balance
+    try {
+      const payeeWorker = await this.queryNodeClient.workersByAccount(payee);
+      this.channels[section].forEach((ch: TextChannel) =>
+        ch.send({
+          embeds: [
+            getDiscretionarySpendingEmbed(spendingAmount, payeeWorker, payload.block, payload.event),
+          ],
+        }));
+    } catch(e) {
+      this.channels[section].forEach((ch: TextChannel) =>
+        ch.send({
+          embeds: [
+            getDiscretionarySpendingToNonWorkerAddressEmbed(spendingAmount, payee, payload.block, payload.event),
+          ],
+        }));      
+    }
+  }
+}
