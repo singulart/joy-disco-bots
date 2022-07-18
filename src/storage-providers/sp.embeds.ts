ddec6a5b46e10c6de73db78de44a4872015c2f90
singulart <serpian@gmail.com>
2022-07-18 12:59:16 +0300

    manual caching

--- src/storage-providers/sp.embeds.ts
+++ src/storage-providers/sp.embeds.ts
@@ -1,7 +1,6 @@
 import Discord, { Role } from 'discord.js';
 import { joystreamBlue } from '../../config'
 
-// TODO Discord.MessageEmbed doesn't support mentions! 
 export const getFaultyNodesEmbed = (summary: string, serverRoleToMention: Role | undefined): Discord.MessageEmbed => {
   return new Discord.MessageEmbed()
     .setTitle(`🚨🚨🚨 Failing storage node(s) alert!`)
